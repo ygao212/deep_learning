backprop fixed

backprop.py
@@ -12,10 +12,61 @@ def classification_rate(Y, P):
 	n_correct = 0
 	n_total = 0
 	for i in xrange(len(Y)):
-		n_total += 1 
+		n_total += 1
 		if Y[i] == P[i]:
-				n_correct += 1
-	return float(n_correct / n_total)
+			n_correct += 1
+	return float(n_correct) / n_total
+
+def derivative_w2(Z, T, Y):
+	N, K = T.shape
+	M = Z.shape[1]
+
+	# ret1 = np.zeros((M, K))
+	# for n in xrange(N):
+	# 	for m in xrange(M):
+	# 		for k in xrange(K):
+	# 			ret1[m,k] += (T[n,k] - Y[n,k]) * Z[n,m]
+	# return ret1
+
+	# ret2 = np.zeros((M, K))
+	# for n in xrange(N):
+	# 	for k in xrange(K):
+	# 		ret2[:, k] += (T[n,k] - Y[n,k]) * Z[n,:]
+	# assert(np.abs(ret1-ret2).sum() < 10e-10)
+
+	# ret3 = np.zeros((M, K))
+	# for n in xrange(N):
+	# 	ret3 += np.outer(Z[n], T[n] - Y[n])
+	# assert(np.abs(ret2 - ret3).sum() < 10e-10)
+
+	# ret4 = Z.T.dot(T-Y)
+	# assert(np.abs(ret4 - ret3).sum() < 10e-10)
+	return Z.T.dot(T - Y)
+
+def derivative_b2(T, Y):
+	return (T - Y).sum(axis=0)
+
+def derivative_w1(X, Z, T, Y, W2):
+	# N, D = X.shape
+	# M, K = W2.shape
+
+	# ret1 = np.zeros((D,M))
+	# for n in xrange(N):
+	# 	for k in xrange(K):
+	# 		for m in xrange(M):
+	# 			for d in xrange(D):
+	# 				ret1[d,m] += (T[n,k] - Y[n,k])*W2[m,k]*Z[n,m]*(1 - Z[n,m])*X[n,d]
+	# return ret1
+	dZ = (T-Y).dot(W2.T) * Z * (1-Z)
+	return X.T.dot(dZ)
+
+def derivative_b1(T, Y, W2, Z):
+	return ((T-Y).dot(W2.T) * Z * (1 -Z)).sum(axis = 0)	
+
+
+def cost(T, Y):
+	tot = T * np.log(Y)
+	return tot.sum()
 
 def main():
 	Nclass = 500
@@ -42,9 +93,24 @@ def main():
 	W2 = np.random.randn(M, K)
 	b2 = np.random.randn(K)
 
-	output, hidden = forward(X, W1, b1, W2, b2)
-	P = np.argmax(output, axis=1)
-	R = classification_rate(Y, P)
+	learning_rate = 10e-7
+	costs = []
+	for epoch in xrange(100000):
+		output, hidden = forward(X, W1, b1, W2, b2)
+		if epoch % 100 == 0:
+			c = cost(T, output)
+			P = np.argmax(output, axis=1)
+			r = classification_rate(Y, P)
+			print "cost:", c, "classification_rate:", r
+			costs.append(c)
+
+		W2 += learning_rate * derivative_w2(hidden, T, output)
+		b2 += learning_rate * derivative_b2(T, output)
+		W1 += learning_rate * derivative_w1(X, hidden, T, output, W2)
+		b1 += learning_rate * derivative_b1(T, output, W2, hidden)
+
+	plt.plot(costs)
+	plt.show()
 	
 	plt.scatter(X[:,0], X[:,1], c=P, s=100, alpha=0.5)
 	plt.show()

process.py
@@ -20,7 +20,7 @@ def get_data():
 		X2[n, t+D-1] = 1
 
 	Z = np.zeros((N, 4))
-	Z[np.arragnge(N), X[:, D-1].astype(np.int32)] = 1
+	Z[np.arange(N), X[:, D-1].astype(np.int32)] = 1
 	assert(np.abs(X2[:, -4:] - Z).sum() < 10e-10)
 	return X2, Y
 